95ef8288608c37839d129a50ec800db9a81a8418
nifemisoyemi <adenifemisoyemi@hotmail.com>
2026-07-20 15:21:14 -0500

    updated homepage and refactor base template

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,7 @@ USE_TZ = True
 # --- Static & media ------------------------------------------------------
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"      # local only; DO Spaces takes over in Milestone 2
 
